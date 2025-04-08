545c7e00edf9a6c0822eadfaaefe92dbbc109ea7
James Houghton <james@jwhoughton.co.uk>
2025-04-08 19:24:49 +0100

    Add leap

--- init.lua
+++ init.lua
@@ -1018,6 +1018,25 @@ require('lazy').setup({
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
   },
+  {
+    'ggandor/leap.nvim',
+    enabled = true,
+    keys = {
+      { 's', mode = { 'n', 'x', 'o' }, desc = 'Leap Forward to' },
+      { 'S', mode = { 'n', 'x', 'o' }, desc = 'Leap Backward to' },
+      { 'gs', mode = { 'n', 'x', 'o' }, desc = 'Leap from Windows' },
+    },
+    config = function(_, opts)
+      local leap = require 'leap'
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ 'x', 'o' }, 'x')
+      vim.keymap.del({ 'x', 'o' }, 'X')
+    end,
+  },
+  { 'tpope/vim-repeat', event = 'VeryLazy' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
