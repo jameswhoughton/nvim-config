e9c3bfe699e2eae0c4cccac7b3b76f4915ce425c
James Houghton <james@jwhoughton.co.uk>
2026-01-17 20:54:20 +0000

    Terraform config

--- init.lua
+++ init.lua
@@ -640,6 +640,7 @@ require('lazy').setup({
             },
           },
         },
+        terraformls = {}
       }
 
       -- Ensure the servers and tools above are installed
@@ -894,7 +895,21 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'terraform',
+        'hcl'
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
